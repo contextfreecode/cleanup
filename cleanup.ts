a130f1214d13ed60ebcebd6810349ade2d95d285
Tom <tjpalmer@pop-os.localdomain>
2020-10-02 21:40:35 -0700

    Add some links to empty ts

--- cleanup.ts
+++ cleanup.ts
@@ -1,2 +1,5 @@
 // Except use js for use from browser.
 console.log("Hi!");
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+// https://developer.mozilla.org/en-US/docs/Web/API/ReadableStreamDefaultController/close
